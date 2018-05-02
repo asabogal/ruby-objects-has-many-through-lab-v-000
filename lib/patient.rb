4272cc10ba2d3e3de94abac6295fd110129e1acd
Alejandro Sabogal <asabogal.git@gmail.com>
2018-05-02 00:34:25 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -8,7 +8,10 @@ attr_accessor :name
   def add_appointment(appointment)
     @appointments << appointment
     appointment.patient = self
-    
+  end
+
+  def appointments
+    @appointments
   end
 
 end
