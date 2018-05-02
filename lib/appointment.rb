4c29050e4377d77ab7c64575ab743408c938c06c
Alejandro Sabogal <asabogal.git@gmail.com>
2018-05-02 00:28:57 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,10 +1,10 @@
 class Appointment
 
-  attr_accessor :doctor, :patient
+  attr_accessor :doctor, :patient, :date
   def initialize(date, doctor)
     @date = date
     @doctor = doctor
-  
+
   end
 
 
